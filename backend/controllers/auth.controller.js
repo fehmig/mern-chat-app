e14636727b686e589d18b648a054c3c39429b241
Fehmi Günay <gunay.fehmi14@gmail.com>
2024-02-19 22:23:49 +0300

    first commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+export const signup = (req, res) => {
+   try {
+    const {fullName, username, password, confirmPassword, gender} = req.body;
+   } catch (error){
+
+   }
+}
+
+export const login = (req, res) => {
+    res.send("Login User")
+    console.log("loginUser");
+}
+
+export const logout = (req, res) => {
+    res.send("Logout User")
+    console.log("logoutUser");
+}
+
